3a1284fdd85d098f8db807afb9da400929df96da
miguel@cimne.upc.edu <miguel@cimne.upc.edu@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2015-09-10 15:29:49 +0000

    Former-commit-id: 22d049d5008dd29971ee07bd2a18df409434aa82

--- modules/Storage/header/abstractAcces.h
+++ modules/Storage/header/abstractAcces.h
@@ -12,11 +12,11 @@ using namespace std;
 class abstractAcces
 {
 public:
-     int create(char *path, char *filename, char *data) = 0;
-     const char* cat(char *path, char *filename) = 0;
-     const char* grep(char *path, char *filename, char *dataToFind) = 0;
+     virtual int create(char *path, char *filename, char *data) = 0;
+     virtual const char* cat(char *path, char *filename) = 0;
+     virtual const char* grep(char *path, char *filename, char *dataToFind) = 0;
     
-     const char* GetResultOnVerticesId(
+     virtual const char* GetResultOnVerticesId(
                                   char* session_Id,
                                   char* model_Id,
                                   char* analysis_Id,
