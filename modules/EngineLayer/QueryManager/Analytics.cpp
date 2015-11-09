0d695acdd67094d3536ddc460f3638e694cf0fee
miguel@cimne.upc.edu <miguel@cimne.upc.edu@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2015-11-09 17:32:02 +0000

    put out common parts of GetBoundingBox java/yarn using local bbox/error.txt and bbox/part-r-00000 for error and result communication to the analytics module (from yarn they are copied with hdfs dfs -copy...) deleting temporay files first,
    forwarding modelID and Tablenames to MapReduce job and mode error verbosity
    
    
    
    
    Former-commit-id: 346d74e1a37aeed797801f565ae6f21622735f4d

--- modules/EngineLayer/QueryManager/Analytics.cpp
+++ modules/EngineLayer/QueryManager/Analytics.cpp
@@ -20,6 +20,23 @@ AnalyticsModule *AnalyticsModule::getInstance() {
   return m_pInstance;
 }
 
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 500
+#endif
+#include <stdio.h>
+#include <ftw.h>
+#include <unistd.h>
+
+static int unlink_cb( const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+  int rv = remove( fpath);
+  // if ( rv)
+  //   perror( fpath);
+  return rv;
+}
+static int recursive_rmdir( const char *path) {
+  return nftw( path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+}
+
 void AnalyticsModule::calculateBoundingBox( const std::string &sessionID, const std::string &modelID, 
 					    const std::string &dataTableName,
 					    const std::string &analysisID, const int numSteps, const double *lstSteps,
@@ -32,74 +49,111 @@ void AnalyticsModule::calculateBoundingBox( const std::string &sessionID, const 
   // return_bbox[ 4] =  0.7;
   // return_bbox[ 5] =  0.7;
   // *return_error_str = "Ok";
+
   // at the moment only CLI interface:
+  // modelID, if it's binary, convert it to 32-digit hexastring:
+  char model_ID_hex_string[ 1024];
+  std::string cli_modelID = ModelID_DoHexStringConversionIfNecesary( modelID, model_ID_hex_string, 1024);
+
+  // remove local
+  // MR output in '../bbox/_SUCCESS'
+  // output in '../bbox/part-r-00000'
+  // error in '../bbox/error.txt'
+  // unlink( "bbox/part-r-00000");
+  // unlink( "bbox/error.txt");
+  // unlink( "bbox/_SUCCESS");
+  // rmdir( "bbox");
+  // delete local temporary files
+  recursive_rmdir( "bbox");
 
   bool use_yarn = true;
   // running java:
   if ( !use_yarn) {
-    char cmd_line[ 1024];
-    sprintf( cmd_line, "java -jar ../Analytics/GetBoundingBoxOfAModel.jar %s %s", modelID.c_str(), dataTableName.c_str());
-    int ret = system( cmd_line);
-    // output in '../bbox/part-r-00000'
-    FILE *fi = fopen( "bbox/part-r-00000", "r");
-    if ( !fi) {
-      *return_error_str = std::string( "Problems executing ") + __FUNCTION__ + std::string( " job.");
-      return;
-    }
-    char keyword[ 1024];
-    double value = 0.0;
-    int num_values = 0;
-    for ( int i = 0; i < 6; i++) {
-      int n = fscanf( fi, "%s %lf", keyword, &value);
-      if ( n == 2) {
-	if (      !strcasecmp( keyword, "min_x")) { return_bbox[ 0] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "min_y")) { return_bbox[ 1] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "min_z")) { return_bbox[ 2] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "max_x")) { return_bbox[ 3] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "max_y")) { return_bbox[ 4] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "max_z")) { return_bbox[ 5] = value; num_values++; }
-      }
-    }
-    fclose( fi);
-    if ( num_values != 6) {
-      *return_error_str = std::string( "Problems with ") + FUNCTION_NAME + std::string( " results.");
-    }
-
-  } else {
+    std::string cmd_line = "java -jar ../Analytics/GetBoundingBoxOfAModel.jar " + cli_modelID + " " + dataTableName;
+    DEBUG( cmd_line);
+    int ret = system( cmd_line.c_str());
+  } else { 
+    // Using yarn:
+    // execute and copy to localdir the result's files
     // running Yarn:
     std::string hadoop_home = "/localfs/home/velassco/common";
     std::string hadoop_bin = hadoop_home + "/hadoop/bin";
-
-    std::string cmd_line = hadoop_bin + "/yarn jar ../Analytics/GetBoundingBoxOfAModel.jar " + modelID + " " + dataTableName;
+    std::string cmd_line = hadoop_bin + "/yarn jar ../Analytics/GetBoundingBoxOfAModel.jar " + cli_modelID + " " + dataTableName;
     int ret = 0;
+    DEBUG( cmd_line);
     ret = system( cmd_line.c_str());
     // output in '../bbox/part-r-00000' but in hdfs
+    // error in '../bbox/error.txt'
     // copy it to local:
-    cmd_line = hadoop_bin + "/hdfs dfs -copyToLocal bbox/part-r-00000 .";
+    cmd_line = hadoop_bin + "/hdfs dfs -copyToLocal bbox .";
     ret = system( cmd_line.c_str());
-    FILE *fi = fopen( "part-r-00000", "r");
-    if ( !fi) {
-      *return_error_str = std::string( "Problems executing ") + __FUNCTION__ + std::string( " job.");
-      return;
-    }
-    char keyword[ 1024];
-    double value = 0.0;
-    int num_values = 0;
-    for ( int i = 0; i < 6; i++) {
-      int n = fscanf( fi, "%s %lf", keyword, &value);
-      if ( n == 2) {
-	if (      !strcasecmp( keyword, "min_x")) { return_bbox[ 0] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "min_y")) { return_bbox[ 1] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "min_z")) { return_bbox[ 2] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "max_x")) { return_bbox[ 3] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "max_y")) { return_bbox[ 4] = value; num_values++; }
-	else if ( !strcasecmp( keyword, "max_z")) { return_bbox[ 5] = value; num_values++; }
+  }
+
+  // output in '../bbox/part-r-00000'
+  // error in '../bbox/error.txt'
+  FILE *fi = fopen( "bbox/part-r-00000", "r");
+  if ( !fi) {
+    // try reading error file
+    bool errorfile_read = false;
+    fi = fopen( "bbox/error.txt", "r");
+    if ( fi) {
+      const size_t size_buffer = 1024 * 1024;
+      char buffer[ size_buffer + 1];
+      char *ok = fgets( buffer, size_buffer, fi);
+      fclose( fi);
+      if ( ok) {
+	*return_error_str = std::string( buffer);
+	errorfile_read = true;
       }
     }
-    fclose( fi);
-    if ( num_values != 6) {
-      *return_error_str = std::string( "Problems with ") + FUNCTION_NAME + std::string( " results.");
+    if ( !errorfile_read) {
+      *return_error_str = std::string( "Problems executing ") + __FUNCTION__ + 
+	( use_yarn ? " Yarn" : " Java") + std::string( " job.");
     }
-    unlink( "part-r-00000");
+    return;
   }
+  // read output
+  char keyword[ 1024];
+  double value = 0.0;
+  int num_values = 0;
+  for ( int i = 0; i < 6; i++) {
+    int n = fscanf( fi, "%s %lf", keyword, &value);
+    if ( n == 2) {
+      if (      !strcasecmp( keyword, "min_x")) { return_bbox[ 0] = value; num_values++; }
+      else if ( !strcasecmp( keyword, "min_y")) { return_bbox[ 1] = value; num_values++; }
+      else if ( !strcasecmp( keyword, "min_z")) { return_bbox[ 2] = value; num_values++; }
+      else if ( !strcasecmp( keyword, "max_x")) { return_bbox[ 3] = value; num_values++; }
+      else if ( !strcasecmp( keyword, "max_y")) { return_bbox[ 4] = value; num_values++; }
+      else if ( !strcasecmp( keyword, "max_z")) { return_bbox[ 5] = value; num_values++; }
+    }
+  }
+  fclose( fi);
+  // verify output:
+  if ( num_values != 6) {
+    // try reading error file
+    bool errorfile_read = false;
+    fi = fopen( "bbox/error.txt", "r");
+    if ( fi) {
+      const size_t size_buffer = 1024 * 1024;
+      char buffer[ size_buffer + 1];
+      char *ok = fgets( buffer, size_buffer, fi);
+      fclose( fi);
+      if ( ok) {
+	*return_error_str = std::string( buffer);
+	errorfile_read = true;
+      }
+    }
+    if ( !errorfile_read) {
+      *return_error_str = std::string( "Problems with ") + FUNCTION_NAME + 
+	( use_yarn ? " Yarn" : " Java") + std::string( " results.");
+    }
+  }
+
+  if ( use_yarn) {
+    // cmd_line = hadoop_bin + "/hdfs dfs -rmdir --ignore-fail-on-non-empty bbox";
+    // ret = system( cmd_line.c_str());
+  }
+
+  // delete local tmp files ...
+  // recursive_rmdir( "bbox");
 }
