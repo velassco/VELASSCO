a439cb0336f2edb592165a1986f51a49aca1a675
olav.liestol@jotne.com <olav.liestol@jotne.com@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2015-11-24 16:36:39 +0000

    Former-commit-id: 0b7d27137608d39ebeb4d6974a202ac6a91923eb

--- EDM_Interface/EDMcluster.hpp
+++ EDM_Interface/EDMcluster.hpp
@@ -1,8 +1,8 @@
-#ifndef EDMcluster_HPP
-#define EDMcluster_HPP
+#ifndef edmcluster_HPP
+#define edmcluster_HPP
 #pragma warning( disable : 4091 )
 
-extern tEdmiEntityData EDMcluster_Entities[];
+extern tEdmiEntityData edmcluster_Entities[];
 
 namespace ecl {
 
@@ -220,8 +220,8 @@ extern void *supertype_cast(entityType wantedSuperType, void *p, entityType subT
 
 extern dbInstance *dbInstance_cast(void *p, entityType subType);
 
-extern const char *EDMcluster_DefinedTypeNames[];
+extern const char *edmcluster_DefinedTypeNames[];
 
-extern ecl_Schema EDMcluster_SchemaObject;
+extern ecl_Schema edmcluster_SchemaObject;
 }
 #endif
