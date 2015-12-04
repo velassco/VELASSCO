bb0d6613c1ca9b297aab366067a5fcceab555aef
olav.liestol@jotne.com <olav.liestol@jotne.com@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2015-12-04 13:38:28 +0000

    Former-commit-id: d30e7ed672ba1ccbf4b813b34a12e3bd9025ad46

--- EDM_plug_in/scripts/InjectAllModels.cmd
+++ EDM_plug_in/scripts/InjectAllModels.cmd
@@ -1,4 +1,4 @@
-O:\projects\VELaSSCo\SVN_src\EDM_injector\DEM_injector\DEM_ingest_server\x64\Debug\DEM_inject_server.exe O:\projects\VELaSSCo\prototype\testdata\VELaSSCo_HbaseBasicTest_FEM\VELaSSCo_HbaseBasicTest_FEM_params_1.txt
-O:\projects\VELaSSCo\SVN_src\EDM_injector\DEM_injector\DEM_ingest_server\x64\Debug\DEM_inject_server.exe O:\projects\VELaSSCo\prototype\testdata\Telescope_128subdomains_ascii\telescope_params.txt
+O:\projects\VELaSSCo\SVN_src\EDM_injector\DEM_injector\DEM_ingest_server\x64\Debug\DEM_inject_server.exe O:\projects\VELaSSCo\prototype\testdata\VELaSSCo_HbaseBasicTest_FEM\VELaSSCo_HbaseBasicTest_FEM_params_1.txt batchMode
+O:\projects\VELaSSCo\SVN_src\EDM_injector\DEM_injector\DEM_ingest_server\x64\Debug\DEM_inject_server.exe O:\projects\VELaSSCo\prototype\testdata\Telescope_128subdomains_ascii\telescope_params.txt batchMode
 
 
