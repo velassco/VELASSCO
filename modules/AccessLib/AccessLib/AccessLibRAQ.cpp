f62bb6bab87180c5b64e2402b95492d9787421e1
miguel@cimne.upc.edu <miguel@cimne.upc.edu@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-01-08 14:02:35 +0000

    nullptr not present in std=c++0x, so using NULL instead
    Former-commit-id: bb4c9627e67200b780abebf3b8e677cda1186b65

--- modules/AccessLib/AccessLib/AccessLibRAQ.cpp
+++ modules/AccessLib/AccessLib/AccessLibRAQ.cpp
@@ -73,24 +73,24 @@ extern "C" {
 	  {
 	    size_t numVertices;
 	    size_t numElements;
-
+	    
 	    std::istringstream in(*queryData);
 	    in >> numVertices >> numElements >> std::ws;
-
-		  if(numVertices > 0 && numElements > 0){
-	        const size_t offsetVertexIDs = (size_t)in.tellg();
-	        const size_t offsetValues    = offsetVertexIDs + numVertices*sizeof(int64_t);
-
-	        *resultVertexIDs   = (const int64_t*)(&((*queryData)[offsetVertexIDs]));
-	        *resultValues      = (const double*) (&((*queryData)[offsetValues]));
-	        *resultNumVertices = numVertices;
-		  } else {
-          *resultVertexIDs   = nullptr;
-          *resultValues      = nullptr;
-          *resultNumVertices = 0;
-      }
+	    
+	    if(numVertices > 0 && numElements > 0){
+	      const size_t offsetVertexIDs = (size_t)in.tellg();
+	      const size_t offsetValues    = offsetVertexIDs + numVertices*sizeof(int64_t);
+	      
+	      *resultVertexIDs   = (const int64_t*)(&((*queryData)[offsetVertexIDs]));
+	      *resultValues      = (const double*) (&((*queryData)[offsetValues]));
+	      *resultNumVertices = numVertices;
+	    } else {
+	      *resultVertexIDs   = NULL;//nullptr;
+	      *resultValues      = NULL;//nullptr;
+	      *resultNumVertices = 0;
+	    }
 	  }
-
+	
 	return result;
       }
     CATCH_ERROR;
