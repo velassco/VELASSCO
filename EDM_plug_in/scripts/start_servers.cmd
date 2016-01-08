df82a0694b3f7af8d7dd2d7fa6ed0124543a0ca3
olav.liestol@jotne.com <olav.liestol@jotne.com@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-01-08 12:19:27 +0000

    Former-commit-id: 89c9920c6487e6185539b3cb5bd1869822a06c59

--- EDM_plug_in/scripts/start_servers.cmd
+++ EDM_plug_in/scripts/start_servers.cmd
@@ -5,11 +5,11 @@ SET BASE_FOLDER=O:\projects\VELaSSCo\SVN_src\EDM_plug_in\db_cluster
 
 set EDM_SERVER_CLIENTS_FILES_PATH=O:\projects\VELaSSCo\SVN_src\EDM_plug_in\db_cluster\temp
 
-start /min %EDM_BIN%\edmserver.exe -ul10007 -o -w -s9090 -l"%BASE_FOLDER%\db1" -nVELaSSCo -pVELaSSCo
+start /min %EDM_BIN%\edmserver.exe -ul10007 -o -w -s9090 -l"%BASE_FOLDER%\db1" -nVELaSSCo -pv
 start /min %EDM_BIN%\edmappserver.exe -w -s9090
 
-start /min %EDM_BIN%\edmserver.exe -ul10175 -o -w -s9080 -l"%BASE_FOLDER%\db2" -nVELaSSCo -pVELaSSCo
+start /min %EDM_BIN%\edmserver.exe -ul10175 -o -w -s9080 -l"%BASE_FOLDER%\db2" -nVELaSSCo -pv
 start /min %EDM_BIN%\edmappserver.exe -w -s9080
 
-start /min %EDM_BIN%\edmserver.exe -ul10176 -o -w -s9070 -l"%BASE_FOLDER%\db3" -nVELaSSCo -pVELaSSCo
+start /min %EDM_BIN%\edmserver.exe -ul10176 -o -w -s9070 -l"%BASE_FOLDER%\db3" -nVELaSSCo -pv
 start /min %EDM_BIN%\edmappserver.exe -w -s9070
