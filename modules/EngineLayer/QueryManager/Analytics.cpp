85f06af889b165ad08274ef653446592f423c290
miguel@cimne.upc.edu <miguel@cimne.upc.edu@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-01-21 14:05:15 +0000

    GetBoundaryOfAMesh serial still
    Former-commit-id: 6fa4251f2f3dab51ef17ecbfd60b464df9349527

--- modules/EngineLayer/QueryManager/Analytics.cpp
+++ modules/EngineLayer/QueryManager/Analytics.cpp
@@ -648,7 +648,7 @@ void AnalyticsModule::calculateBoundaryOfAMesh( const std::string &sessionID, co
   //GetBoundaryOfAMesh/dist/GetBoundaryOfAMesh.jar 1 60069901000000006806990100000000 Simulations_Data_V4CIMNE 1 Tetrahedra static
   std::string analytics_program = GetFullAnalyticsQualifier( "GetBoundaryOfAMesh");
 
-  bool use_yarn = true;;//true;
+  bool use_yarn = false;;//true;
   // running java:
   int ret_cmd = 0;
   char meshIDstr[ 100];
