d496db3aaeed0ed48d5a9de245ce10bd4da176cf
olav.liestol@jotne.com <olav.liestol@jotne.com@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-02-05 08:30:12 +0000

    Changed rvGetCoordinatesAndElementsFromMesh to use binary for bulk data.
    Former-commit-id: 4db4024953750c371a36bc19c25e8feee2175d53

--- modules/thrift/Storage/VELaSSCoSM.thrift
+++ modules/thrift/Storage/VELaSSCoSM.thrift
@@ -269,6 +269,11 @@ struct rvGetResultFromVerticesID {
   6: binary             result_array
 }
 
+
+/*
+   This rvGetCoordinatesAndElementsFromMesh is exchanged with the version below due
+   due to performance problems with this one.
+
 struct rvGetCoordinatesAndElementsFromMesh {
   1: string status
   2: string report
@@ -277,6 +282,23 @@ struct rvGetCoordinatesAndElementsFromMesh {
   5: list<ElementAttrib> element_attrib_list
   6: list<ElementGroup>  element_group_info_list
 }
+*/
+
+struct rvGetCoordinatesAndElementsFromMesh {
+   1  : string                      status
+   2  : string                      report
+   3  : i64                         model_type        // FEM = 1, DEM =2
+   4  : ElementShapeType            element_type
+   5  : i64                         n_vertices
+   6  : i64                         vertex_record_size
+   7  : i64                         n_elements
+   8  : i64                         n_vertices_pr_element
+   9  : i64                         element_record_size
+   10 : binary                      vertex_array
+   11 : binary                      element_array
+}
+
+
 
 // provides service VELaSSCo Storage Module
 service VELaSSCoSM
