808d0d0fba34cdc479cdd11163c4c636f1ab9cf9
ronda@cimne.upc.edu <ronda@cimne.upc.edu@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-10-07 16:13:24 +0000

    Summary: spark cmdline
    almost implemented isosurface, invoking spark job but still not reading the output
    
    
    
    Former-commit-id: 9dcf487e972640e48436e1e55d84575e6bc1efbb

--- modules/Platform/analytics/AnalyticsIsoSurface.cpp
+++ modules/Platform/analytics/AnalyticsIsoSurface.cpp
@@ -1,4 +1,6 @@
+#include <sstream>
 #include "DataLayerAccess.h" // must be included before Analytics
+#include "AnalyticsCommon.h"
 #include "Analytics.h"
 // the format is shared between the QueryManager, the AccessLibrary and th visualiztion client
 // so it's located in the AccessLibrary folder
@@ -17,6 +19,26 @@ void AnalyticsModule::calculateIsoSurface(const std::string &sessionID,
 					  const std::string &resultName, const int resultComp, const double isoValue,
 					  std::string *return_binary_mesh, std::string *return_error_str)
 {
-  *return_error_str = "[calculateIsoSurface] -- unimplemented";
+  std::stringstream cmdline;
+  std::string pathJar(getVELaSSCoBaseDir());
+  pathJar += "/AnalyticsSparkJobs/VelasscoSpark-0.1-SNAPSHOT.jar";
+
+  std::string outputFile("Mesh_IsoSurface_");
+  outputFile += sessionID;
+  outputFile += ".dat";
+
+  std::string logFile("/tmp/Output_IsoSurface_");
+  logFile += sessionID;
+  logFile += ".log";
+  
+  cmdline << "spark-submit --master yarn --num-executors 40 --class com.cimne.velassco.ComputeIsoSurfaceApp "
+	  << pathJar << " --model_id " << modelID << " --analysis " << analysisID << " --timestep " << stepValue
+	  << " --result " << resultName << " --component " << resultComp << " --output_path " << outputFile
+	  << " > " << logFile;
+  int ret = system(cmdline.str().c_str());
+  LOGGER << "[AnalyticsModule::calculateIsoSurface] -- invoking spark job as:\n";
+  LOGGER << cmdline.str() << std::endl;
+  
+  *return_error_str = "[calculateIsoSurface] -- almost implemented";
 }
 
