250cec4ceb9b5f06ccc3c11e70b534d4ae511c56
olav.liestol@jotne.com <olav.liestol@jotne.com@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-11-02 10:00:03 +0000

    removed delete model in FEM_Injectorhandler::setCurrentModel
    Former-commit-id: 50498618ab7b3dd065608f03449b563016849afb

--- modules/Platform/edm_plugin/FEM_InjectorHandler.cpp
+++ modules/Platform/edm_plugin/FEM_InjectorHandler.cpp
@@ -531,7 +531,7 @@ void FEM_InjectorHandler::setCurrentModel(const char *modelName)
 {
    if (m == NULL) {
       m = new Model(currentRepository, &model_ma, currentSchema);
-      EdmiError rstat = edmiDeleteModelBN(currentRepository->getRepositoryId(), (char*)modelName);
+//      EdmiError rstat = edmiDeleteModelBN(currentRepository->getRepositoryId(), (char*)modelName);
       SdaiModel modelID = edmiCreateModelBN(currentRepository->getRepositoryId(), (char*)modelName,
          currentSchemaName, NO_INSTANCE_REFERENCES | M_PACKED_MODEL);
       m->open((char*)modelName, sdaiRW);
