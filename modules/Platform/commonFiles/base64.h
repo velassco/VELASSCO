4cdaf16bef9ac97aac6710b3e77446dcc8fa0c15
miguel@cimne.upc.edu <miguel@cimne.upc.edu@25920a51-a7dd-4700-9a50-0a4dee7aa7ea>
2016-12-13 14:23:34 +0000

    more rebust base64 functions: if parameter is null or empty, return an empty std::string
    Former-commit-id: 7a026f12f6674e9debeb16c8f9a85685a17e584e

--- modules/Platform/commonFiles/base64.h
+++ modules/Platform/commonFiles/base64.h
@@ -6,6 +6,7 @@ std::string base64_encode( char const* );
 std::string base64_encode( char const* , size_t len);
 std::string base64_decode(std::string const& s);
 inline std::string base64_decode( const char *data) {
-  return data ? base64_decode( std::string( data)) : "(null)";
+  // return data ? base64_decode( std::string( data)) : "(null)";
+  return data ? base64_decode( std::string( data)) : "";
 }
 #endif // _BASE_64_H_
